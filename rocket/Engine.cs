da96d1cfb884c1019c65ab4317d99e6b0192cf42
GreatLoofah <uzayr34@hotmail.ca>
2026-05-21 23:14:43 -0600

    Add project files.

---
+++ rocket/Engine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rocket
+{
+    public abstract class Engine
+    {
+        protected string Type;
+        protected string Name;
+        protected double Thrust;
+        protected double Isp;
+        protected double Mass;
+        protected double DryMass;
+
+        public virtual void DisplayStats()
+        {
+
+        }
+
+        public virtual double DisplayThrust()
+        {
+            return -1;
+        }
+    }
+
+    public class Flea : Engine
+    {
+        private new string Type = "solid rocket booster/SRB";
+        private new string Name = "Flea";
+        private new double Thrust = 162909; //newtons
+        private new double Isp = 140;
+        private new double Mass = 1; //placeholder
+        private new double DryMass = 1; //Placeholder
+
+        public override void DisplayStats()
+        {
+            Console.WriteLine($"This is the {Name} {Type}, it has a thrust of {Thrust} newtons, a specific impulse of {Isp}");
+        }
+
+        public override double DisplayThrust()
+        {
+            return Thrust;
+        }
+    }
+
+}
---
+++ rocket/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rocket
+{
+    public class Rocket
+    {
+        //=======================================CONSTRUCTORS=======================================
+        public double G = 9.81;
+        public double Velocity = 0;
+        public double Mass;
+        public double MassLoss;
+        public double Time;
+        public double Drag;
+        public double Thrust;
+
+        public Rocket() //control conditions
+        {
+            Mass = 1396;
+            MassLoss = 131.25;
+            Time = 1;
+            Drag = 0.3;
+            Thrust = 162909;
+        }
+
+        public Rocket(double mass, double massLoss, double time, double drag, Engine engine) //overload
+        {
+            Mass = mass;
+            MassLoss = massLoss;
+            Time = time;
+            Drag = drag;
+            Thrust = engine.DisplayThrust();
+        }
+        //=========================================METHODS=============================================
+        public void rocketStats()
+        {
+            Console.WriteLine($"The mass of the rocket is {Mass}kg/{Mass/1000} tons");
+            
+        }
+
+        public void FinalVelocity()
+        {
+            for(int i = 0; i < Time; i++)
+            {
+                Velocity = Velocity + ((Thrust - (Mass - MassLoss * i) * G - (Drag*(Velocity*Velocity)) + (MassLoss*Velocity)) / (Mass - (MassLoss*i)));
+            }
+            Console.WriteLine(Velocity);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine f = new Flea();
+
+            Rocket r = new Rocket(1, 1, 1, 1, f);
+            Console.WriteLine(r.Thrust);
+        }
+    }
+}
